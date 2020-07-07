adbf0e26f6893d846b57f91eae96e3990c3ed0ff
Leah Riffell <leah.riffell@gmail.com>
2020-07-06 19:47:03 -0600

    Create cell can be fired upon method and test

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,9 +1,10 @@
 class Cell
-  attr_reader :coordinate, :ship
+  attr_reader :coordinate, :ship, :fired_upon
 
-  def initialize(coordinate, ship = nil)
+  def initialize(coordinate, ship = nil, fired_upon = false)
     @coordinate = coordinate
     @ship = ship
+    @fired_upon = fired_upon
   end
 
   def empty?
@@ -16,5 +17,17 @@ class Cell
 
   def place_ship(ship)
     @ship = ship
+  end
+
+  def fire_upon
+    # fire upon will decrement ship health and also change fired_upon to true
+    @fired_upon = true
+    if @ship != nil
+      @ship.hit
+    end
+  end
+
+  def fired_upon?
+    @fired_upon
   end
 end
