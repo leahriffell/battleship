9a8b7a8b643e0a5683a866cbd79d5b5e449e5d34
Nico Rithner <nicorithner@gmail.com>
2020-07-06 18:31:15 -0600

    Add test_it_can_be_sunk initial assertions

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -4,17 +4,29 @@ require './lib/ship'
 require "pry"
 
 class ShipTest < Minitest::Test
-    def test_it_exists
-      ship = Ship.new("Cruiser", 3)
-      assert_instance_of Ship, ship
-    end
+  def test_it_exists
+    ship = Ship.new("Cruiser", 3)
+    assert_instance_of Ship, ship
+  end
 
-    def test_it_has_attributes
-        ship = Ship.new("Cruiser", 3)
+  def test_it_has_attributes
+    ship = Ship.new("Cruiser", 3)
 
-        assert_equal "Cruiser", ship.name
-        assert_equal 3, ship.length
-        assert_equal 3, ship.health
-        assert_equal false, ship.sunk?
-      end
+    assert_equal "Cruiser", ship.name
+    assert_equal 3, ship.length
+    assert_equal 3, ship.health
+    assert_equal false, ship.sunk?
+  end
+
+  def test_it_can_be_sunk
+    ship = Ship.new("Cruiser", 3)
+
+    cruiser.hit
+    assert_equal 2, cruiser.health
+    cruiser.hit
+    assert_equal 1, cruiser.health
+    assert_equal false, cruiser.sunk?
+    cruiser.hit
+    assert_equal true, cruiser.sunk?
+  end
 end
