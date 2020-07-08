18c65bbd44f5c07d47fe08399f9064ba54a98dde
Nico Rithner <nicorithner@gmail.com>
2020-07-07 18:15:08 -0600

    Add generate coordinate keys array to be used in cells hash

--- test/board_test.rb
+++ test/board_test.rb
@@ -12,3 +12,11 @@ class BoardTest < Minitest::Test
     board = Board.new
     assert_instance_of Board, board
   end
+
+  def test_it_has_valid_coordinates
+    board = Board.new
+    board.generate_board_columns_and_rows
+    # assert_instance_of true, board.valid_coordinate?("A1")
+    # assert_equal false, board.valid_coordinate?("A5")
+  end
+end
