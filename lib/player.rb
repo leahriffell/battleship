d5ec45b401e0dfac8784ac674822cad25fd1d1c9
Leah Riffell <leah.riffell@gmail.com>
2020-07-09 17:40:30 -0600

    Generate ships and assign to computer and human players

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,20 @@
+require "./lib/ship"
+
 class Player
-  attr_reader :type
-  
+  attr_reader :type, :human_ships, :computer_ships
+
   def initialize(type)
     @type = type
+    @human_ships = []
+    @computer_ships = []
+    generate_ships
+  end
+
+  def generate_ships
+    @human_ships << Ship.new("Cruiser", 3)
+    @human_ships << Ship.new("Submarine", 2)
+    # binding.pry
+    @computer_ships << Ship.new("Cruiser", 3)
+    @computer_ships << Ship.new("Submarine", 2)
   end
 end
--- test/player_test.rb
+++ test/player_test.rb
@@ -6,16 +6,23 @@ require "pry"
 
 class GameTest < MiniTest::Test
   def test_it_exists
-    player = Player.new("human")
-
+    player = Player.new("Human")
     assert_instance_of Player, player
   end
 
-  def test_player_type
-    human = Player.new("human")
-    computer = Player.new("computer")
+  def test_it_has_a_type
+    human = Player.new("Human")
+    computer = Player.new("Computer")
 
-    assert_equal "human", human.type
-    assert_equal "computer", computer.type
+    assert_equal "Human", human.type
+    assert_equal "Computer", computer.type
+  end
+
+  def test_it_has_two_ships
+    human = Player.new("Human")
+    computer = Player.new("Computer")
+
+    assert_equal 2, human.human_ships.length
+    assert_equal 2, computer.computer_ships.length
   end
 end
