629dd57ceca09d4c200e49c31dc68786c27cf2fc
Leah Riffell <leah.riffell@gmail.com>
2020-07-11 15:39:38 -0600

    Add method and test to render board (not yet working)

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,2 +1,16 @@
 class Turn
+  attr_reader :computer, :human
+
+  def initialize(computer, human)
+    @computer = computer
+    @human = human
+  end
+
+  def display_boards
+    # trying to use return instead of puts (like board render method)
+     "=============COMPUTER BOARD=============\n" +
+     "#{@computer.board.render}\n" +
+     "==============PLAYER BOARD==============\n" +
+     "#{@human.board.render}\n"
+  end
 end
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -1,12 +1,23 @@
 require "minitest/autorun"
 require "minitest/pride"
 require "./lib/turn"
+require "./lib/player"
 require "pry"
 
 class TurnTest < MiniTest::Test
   def test_it_exists
-    turn = Turn.new
+    computer = Player.new("Computer")
+    human = Player.new("Human")
+    turn = Turn.new(computer, human)
 
     assert_instance_of Turn, turn
+  end
+
+  def test_it_displays_boards
+    computer = Player.new("Computer")
+    human = Player.new("Human")
+    turn = Turn.new(computer, human)
+
+    assert_equal "=============COMPUTER BOARD=============\n  1 2 3 4 \nA . . . . \nB . . . . \nC . . . . \nD . . . . \n==============PLAYER BOARD==============\n  1 2 3 4 \nA . . . . \nB . . . . \nC . . . . \nD . . . .", turn.display_boards
   end
 end
