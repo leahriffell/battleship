2e602fcdb7842e8f5cd4e815141a1d706aeb82d3
Nico Rithner <nicorithner@gmail.com>
2020-07-15 13:31:41 -0600

    Update tests to reflect changes in the class files

--- test/game_test.rb
+++ test/game_test.rb
@@ -7,19 +7,20 @@ require "pry"
 
 class GameTest < MiniTest::Test
   def test_it_exist
-    game = Game.new
+    game = Game.new("4", "4")
+
     assert_instance_of Game, game
   end
 
   def test_it_has_human_and_computer_player
-    game = Game.new
+    game = Game.new("4", "4")
 
     assert_equal "Human", game.human_player.type
     assert_equal "Computer", game.computer_player.type
   end
 
   def test_it_has_assigned_boards
-    game = Game.new
+    game = Game.new("4", "4")
 
     assert_equal true, game.human_player.has_board?
     assert_equal true, game.computer_player.has_board?
--- test/player_test.rb
+++ test/player_test.rb
@@ -7,49 +7,39 @@ require "pry"
 
 class GameTest < MiniTest::Test
   def test_it_exists
-    player = Player.new("Human")
+    player = Player.new("Human", "4", "4")
     assert_instance_of Player, player
   end
 
   def test_it_has_a_type
-    human = Player.new("Human")
-    computer = Player.new("Computer")
+    human = Player.new("Human", "4", "4")
+    computer = Player.new("Computer", "4", "4")
 
     assert_equal "Human", human.type
     assert_equal "Computer", computer.type
   end
 
   def test_it_has_two_ships
-    human = Player.new("Human")
-    computer = Player.new("Computer")
+    human = Player.new("Human", "4", "4")
+    computer = Player.new("Computer", "4", "4")
 
     assert_equal 2, human.ships.length
     assert_equal 2, computer.ships.length
   end
 
   def test_it_has_a_board
-    human = Player.new("Human")
-    computer = Player.new("Computer")
+    human = Player.new("Human", "4", "4")
+    computer = Player.new("Computer", "4", "4")
 
     assert_equal true, human.has_board?
     assert_equal true, computer.has_board?
   end
 
   def test_computer_player_randomly_places_ships
-    computer = Player.new("Computer")
+    computer = Player.new("Computer", "4", "4")
 
     computer.randomly_place_cruiser
     computer.randomly_place_submarine
     assert_equal 5, computer.board.cells.select {|key, value| value.ship}.length
-  end
-
-  def test_human_can_place_cruiser
-    human = Player.new("Human")
-    # This is similar to our welcome message method that requires user input in order to do execute. We did not test in that case as it'll be caught when we play the game.
-
-    # don't want to call this in our test
-    # human.let_human_place_cruiser
-    # human.let_human_place_submarine
-    # assert_equal 3, human.board.cells.select {|key, value| value.ship}.length
   end
 end
