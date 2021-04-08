988f7c8deeb8e852fd71f7a1fdc32655367a22ec
Leah Riffell <leah.riffell@gmail.com>
2021-04-08 09:04:16 -0400

    Refactor cell class

--- lib/cell.rb
+++ lib/cell.rb
@@ -8,11 +8,7 @@ class Cell
   end
 
   def empty?
-    if @ship == nil
-      return true
-    else
-      false
-    end
+    @ship.nil?
   end
 
   def place_ship(ship)
@@ -20,35 +16,39 @@ class Cell
   end
 
   def fire_upon
-    # fire upon will decrement ship health and also change fired_upon to true
     @fired_upon = true
-    if @ship != nil
-      @ship.hit
-    end
+    @ship&.hit
   end
 
   def fired_upon?
     @fired_upon
   end
 
-  def render(ship_display = false) ## => candidate for refactoring
-    if ship_display == true
-      if @ship != nil
-      "S"
+  def miss
+    @fired_upon && @ship.nil?
+  end
+
+  def sunk
+    @fired_upon && !@ship.nil? && @ship.sunk?
+  end
+
+  def hit
+    @fired_upon && !@ship.nil?
+  end
+
+  def render(ship_display = false)
+    if ship_display
+      empty? ? '.' : 'S'
+    else
+      if miss
+        'M'
+      elsif sunk
+        'X'
+      elsif hit
+        'H'
       else
-        "."
-      end
-    elsif ship_display == false
-      if @fired_upon == true && @ship == nil
-        "M"
-      elsif @fired_upon == true && @ship != nil && @ship.sunk? == true
-        "X"
-      elsif @fired_upon == true && @ship != nil
-        "H"
-      else
-        "."
+        '.'
       end
     end
   end
-
 end
